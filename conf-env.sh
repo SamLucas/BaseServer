f490b57074eef6c1ff2e1ed836aee4b76282ac85
Samuel Lucas Santos Gomes <samuellucas0603@gmail.com>
2019-08-29 16:13:21 -0300

    configuração inicial

--- conf-env.sh
+++
@@ -1,6 +0,0 @@
-# Criando copias de arquivos .env para os diretorios de configuração
-ln -s ./.env services/mysql/conf/.env
-ln -s ./.env services/web/conf/.env
-ln -s ./.env services/traefik/.env
-
-# Configurando os arquivos docker-compose 
